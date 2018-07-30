c0001132ec5712fc6d4bcc424b652512f8c26d2f
Omar Ali <omar.ali.developer@gmail.com>
2018-07-30 11:53:06 +0200

    =

--- server/server.js
+++ server/server.js
@@ -2,9 +2,10 @@ const express = require("express"),
     bodyParser = require("body-parser");
 
 
-var {mongoose} = require("./db/mongoose");
+var {mongoose, ObjectID} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
+var {ObjectID} = require("mongodb");
 
 var app = express();
 
@@ -22,13 +23,29 @@ app.post("/todos", (req, res) => {
         res.status(400).send(e);
     })
 
-})
+});
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+app.get("/users/:id" , (req , res)=> {
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)) {
+       return res.status(404).send({}); 
+    }
+    User.findById(id).then((docs) => {
+        if(!docs) {
+            return res.status(404).send({});
+        }
+        res.send(docs);
+    })
+    .catch((e) => {
+        res.status(400).send({});
+    });
 });
 
 
